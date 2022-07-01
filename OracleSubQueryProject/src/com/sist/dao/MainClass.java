274fb97d8e0bf1326d3ae65a0b181a0ad4f37e4d
ajskd5 <ajskd5@naver.com>
2022-07-01 11:17:34 +0900

    123

--- OracleSubQueryProject/src/com/sist/dao/MainClass.java
+++ OracleSubQueryProject/src/com/sist/dao/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		EmpDAO dao = new EmpDAO();
-		Scanner scan = new Scanner(System.in);
+//		Scanner scan = new Scanner(System.in);
 //		System.out.print("이름 입력 : ");
 //		String ename = scan.next();
 //		List<EmpVO> list = dao.empListData2(ename.toUpperCase()); // 대문자로 저장되어 있어서 입력을 대소문자 구분없이 받을 수 있음
---
+++ OracleViewProject/src/com/sist/dao/EmpDeptDAO.java
@@ -0,0 +1,44 @@
+package com.sist.dao;
+
+import java.sql.*;
+import java.util.*;
+/*
+ * EmpDeptDAO <=============> Oracle (서버)
+ *   client			TCP
+ */
+
+public class EmpDeptDAO {
+	private Connection conn; // Socket
+	private PreparedStatement ps; // BufferedReader / OutputStream
+	// 웹 ==> HttpServletRequest(Socket) / HttpServletResponse(서버)
+	// Spring => Model
+	private final String URL = "jdbc:orcle:thin:@localhost:1521:XE";
+	// 드라이버 등록 => 드라이버 / 서버연결 / 배치 / 멤버변수 초기화 => 생성자
+	public EmpDeptDAO() {
+		try {
+			Class.forName("oracle.jdbc.dirver.OracleDriver");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(URL, "hr", "happy");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	public void disConnection() {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+			if(ps != null) {
+				ps.close();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
